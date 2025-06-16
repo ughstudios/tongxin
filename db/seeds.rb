5eaaaa55b330a5f51bd398a94a69bc0a2ec82432
Daniel Gleason <damianscape@gmail.com>
2025-06-16 13:35:47 -0700

    Fix seed admin password length

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # Create a default admin user if it does not already exist
 User.find_or_create_by!(username: 'admin') do |user|
   user.email = 'admin@example.com'
-  user.password = 'admin'
-  user.password_confirmation = 'admin'
+  user.password = 'admin123'
+  user.password_confirmation = 'admin123'
 end
 
