139f87b6b104f96c4bd8da70d0473911d9d2b205
Daniel Gleason <damianscape@gmail.com>
2025-06-16 14:16:35 -0700

    Integrate feed and recommendations on homepage

--- app/controllers/recommendations_controller.rb
+++
@@ -1,7 +0,0 @@
-class RecommendationsController < ApplicationController
-  def index
-    posts = Post.order(created_at: :desc).limit(20)
-    recommender = LlmPostRecommender.new(current_user)
-    @recommended_posts = posts.select { |post| recommender.interested?(post) }
-  end
-end
