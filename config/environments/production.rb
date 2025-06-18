d6be18b7a2fbc35c9e7ab486c0680860ffe2257e
Daniel Gleason <damianscape@gmail.com>
2025-06-17 17:48:56 -0700

    Serve assets without digest in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,4 +2,10 @@
 
 Rails.application.configure do
   config.eager_load = true
+  # Disable asset fingerprinting so static HTML can reference
+  # non-digested filenames like /assets/application.js in production.
+  config.assets.digest = false
+
+  # Allow on-the-fly compilation if precompiled assets are missing.
+  config.assets.compile = true
 end
