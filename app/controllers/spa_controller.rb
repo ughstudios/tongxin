cb6af32e7e6de00efadb96ca2c5fa9de86b7291d
Daniel Gleason <damianscape@gmail.com>
2025-06-17 17:27:36 -0700

    Serve React assets via Rails

--- app/controllers/spa_controller.rb
+++ app/controllers/spa_controller.rb
@@ -2,6 +2,6 @@ class SpaController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    render file: Rails.root.join('public', 'index.html'), layout: false
+    render template: 'spa/index', layout: false
   end
 end
