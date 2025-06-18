7c50957acadfc38b0a2cd0c7c651c6013d5b8e74
Daniel Gleason <damianscape@gmail.com>
2025-06-18 09:02:30 -0700

    Enable asset compilation in dev and test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,4 +2,6 @@
 
 Rails.application.configure do
   config.eager_load = false
+  # Allow assets like application.js to be served without precompiling
+  config.assets.compile = true
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -2,4 +2,6 @@
 
 Rails.application.configure do
   config.eager_load = false
+  # Allow asset pipeline to compile missing files during tests
+  config.assets.compile = true
 end
