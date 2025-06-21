776adf68b05a4aa284cdfb62020219223cd978f0
Daniel Gleason <damianscape@gmail.com>
2025-06-21 16:38:28 -0700

    Clarify duplicate column check

--- migrations/0006-repost.js
+++ migrations/0006-repost.js
@@ -1,13 +1,20 @@
 'use strict'
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn('Posts', 'repostId', {
-      type: Sequelize.INTEGER,
-      allowNull: true,
-      references: { model: 'Posts', key: 'id' }
-    })
+    // avoid duplicate column errors when re-running migrations
+    const table = await queryInterface.describeTable('Posts')
+    if (!table.repostId) {
+      await queryInterface.addColumn('Posts', 'repostId', {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        references: { model: 'Posts', key: 'id' }
+      })
+    }
   },
   async down(queryInterface) {
-    await queryInterface.removeColumn('Posts', 'repostId')
+    const table = await queryInterface.describeTable('Posts')
+    if (table.repostId) {
+      await queryInterface.removeColumn('Posts', 'repostId')
+    }
   }
 }
