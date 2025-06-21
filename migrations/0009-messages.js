f478fc7d3d06561a3c1a59b09e94587d45f1c4c0
Daniel Gleason <damianscape@gmail.com>
2025-06-20 17:54:16 -0700

    Add migrations for messages and hashtags

---
+++ migrations/0009-messages.js
@@ -0,0 +1,25 @@
+'use strict'
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Messages', {
+      id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+      senderId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'Users', key: 'id' }
+      },
+      receiverId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'Users', key: 'id' }
+      },
+      content: { type: Sequelize.TEXT, allowNull: false },
+      read: { type: Sequelize.BOOLEAN, allowNull: false, defaultValue: false },
+      createdAt: { type: Sequelize.DATE, allowNull: false },
+      updatedAt: { type: Sequelize.DATE, allowNull: false }
+    })
+  },
+  async down(queryInterface) {
+    await queryInterface.dropTable('Messages')
+  }
+}
